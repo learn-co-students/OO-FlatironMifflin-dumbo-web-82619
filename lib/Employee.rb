9b95685ddf4152093e0bb501b0574f876cc4bed4
github username <github email address>
2019-09-01 22:06:55 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,45 @@
 class Employee
+    attr_reader(:name, :salary, :manager_name)
+
+    @@all = []
+
+    def initialize(name, salary, manager_name)
+        @name = name
+        @salary = salary
+        @manager_name = manager_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(salary)
+        found = self.all.select do |employee|
+            employee.salary > salary
+        end
+        if found.size == 0
+            "There is no one paid that high"
+        else
+            found
+        end
+    end
+
+    def self.find_by_department(department)
+        found = Manager.all.find do |manager|
+            manager.department == department
+        end
+        select = self.all.find do |employee|
+            employee.manager_name == found.name
+        end
+    end
+
+    def tax_bracket
+        tax_bracket = Employee.all.select do |employee|
+            employee.salary >= (self.salary - 1000) && employee.salary <= (self.salary + 1000)
+        end
+        tax_bracket.delete(self)
+        tax_bracket
+    end
 
 end
